b8d5febceff0f0a1984fc287a71c3f2dc22abdf3
David <david_rojasavalos@hotmail.com>
2025-03-17 16:44:25 -0600

    fix: modificar el rango de la agenda, tareas completadas y eliminadas

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def main():
             hora = validar_hora("Ingrese la hora de la tarea (HH:MM): ")
             idPrioridad = validar_numero_en_rango("Ingrese el ID de la prioridad (1. ALTA, 2. MEDIA, 3. BAJA): ", 1, 3)
             idCategoria = validar_numero_en_rango("Ingrese el ID de la categoría (1. ESCUELA, 2. TRABAJO, 3. CASA): ", 1, 3)
-            idAgenda = validar_numero_en_rango("Ingrese el ID de la agenda: ", 1, 99)  # Ajusta el rango según tus necesidades
+            idAgenda = validar_numero_en_rango("Ingrese el ID de la agenda: ", 1, 1)  
             agenda.agregar_tarea(descripcion, fecha, hora, idPrioridad, idCategoria, idAgenda)
 
         elif opcion == "2":
             agenda.mostrar_tareas()
 
         elif opcion == "3":
-            idTarea = validar_numero_en_rango("Ingrese el ID de la tarea a modificar: ", 1, 99)  # Ajusta el rango según tus necesidades
+            idTarea = validar_numero_en_rango("Ingrese el ID de la tarea a modificar: ", 1, 999)
             nueva_descripcion = validar_campo_no_vacio("Nueva descripción: ")
             nueva_fecha = validar_fecha("Nueva fecha (YYYY-MM-DD): ")
             nueva_hora = validar_hora("Nueva hora (HH:MM): ")
@@ -32,7 +32,7 @@ def main():
             agenda.modificar_tarea(idTarea, nueva_descripcion, nueva_fecha, nueva_hora, nueva_prioridad, nueva_categoria)
 
         elif opcion == "4":
-            idTarea = validar_numero_en_rango("ID de la tarea a completar: ", 1, 1)
+            idTarea = validar_numero_en_rango("ID de la tarea a completar: ", 1, 999)
             agenda.marcar_completada(idTarea)
 
         elif opcion == "5":
